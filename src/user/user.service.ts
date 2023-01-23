cce1fbdbbdc1f8282b1d9eab0d116787fb13d201
Titux Metal (MBP) <github@lgdweb.fr>
2023-01-23 05:52:40 +0100

    Updating user service; implements create user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,12 +1,29 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, UnauthorizedException } from '@nestjs/common'
+import * as argon from 'argon2'
 
 import { PrismaService } from '~/prisma'
+
+import { UserEntity } from './entity'
+import { CreateUserParams } from './interfaces'
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async create() {
-    return { message: 'User created successfully' }
+  async create({ email, password, username }: CreateUserParams): Promise<UserEntity> {
+    try {
+      const hash = await argon.hash(password)
+      const newUser = { email, hash, username }
+      const user = await this.prisma.user.create({
+        data: newUser
+      })
+
+      return new UserEntity(user)
+    } catch (error) {
+      throw new UnauthorizedException('Invalid Credentials.', {
+        cause: new Error('Unique contstraint'),
+        description: 'Cannot create User.'
+      })
+    }
   }
 }
