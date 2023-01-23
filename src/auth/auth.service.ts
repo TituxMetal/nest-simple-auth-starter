f1ae497e1706ed78be6a87fa3c50023b8fe5df49
Titux Metal (MBP) <github@lgdweb.fr>
2023-01-23 06:05:37 +0100

    Updating auth.service.ts file; implements register user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,17 +3,35 @@ import { ConfigService } from '@nestjs/config'
 import { JwtService } from '@nestjs/jwt'
 
 import { PrismaService } from '~/prisma'
+import { UserService } from '~/user'
+
+import { AuthUserEntity } from './entity'
+import type { RegisterParams, Token, TokenPayload } from './types'
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly config: ConfigService,
     private readonly jwt: JwtService,
-    private readonly prisma: PrismaService
+    private readonly prisma: PrismaService,
+    private readonly userService: UserService
   ) {}
 
-  async registerUser() {
-    return { message: 'User registered successfully' }
+  private async createToken(payload: TokenPayload): Promise<Token> {
+    const accessToken = await this.jwt.signAsync(payload, {
+      secret: this.config.getOrThrow<string>('JWT_SECRET'),
+      expiresIn: this.config.getOrThrow<string>('JWT_EXPIRE')
+    })
+
+    return accessToken
+  }
+
+  async registerUser({ email, password, username }: RegisterParams): Promise<AuthUserEntity> {
+    const newUser = await this.userService.create({ email, password, username })
+    const token = await this.createToken({ sub: newUser.id, email: newUser.email })
+    const registeredUser = new AuthUserEntity({ ...newUser, token })
+
+    return registeredUser
   }
 
   async loginUser() {
